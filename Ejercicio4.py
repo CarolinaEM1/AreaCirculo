242b4765c36213351537d0a6d6a6fb5ce7e9d6ce
Carolina Martinez <carolinamtz091@gmail.com>
2023-05-21 10:58:37 -0600

    primera version

---
+++ Ejercicio4.py
@@ -0,0 +1,11 @@
+#Area y longitud de un círculo
+
+radio = float(input("Digite el radio del círculo: "))
+
+area = (3.1416 *radio**2)
+longitud = (2*3.1416*radio)
+
+print(f"El area del círculo es: {area: .2f}")
+print(f"La longitud del círculo es: {longitud: .2f}") #El .2f son los decimales que quieres mostrar en el resultado
+
+#Carolina EM
